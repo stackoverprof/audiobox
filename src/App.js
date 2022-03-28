e2156bb5c32ca0dda96a27aac16dafe2be07de4b
errbint <mail.errbint@gmail.com>
2022-03-28 16:49:36 +0700

    Task mod2 ses3

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import "./styles.css";
-import data from "./data/song";
+import tracks_data from "./data/tracks";
 import TrackCard from "./components/TrackCard";
 
 export default function App() {
   return (
     <div className="App">
-      <TrackCard
-        img={data.album.images[0].url}
-        name={data.name}
-        artists={data.artists}
-        album_name={data.album.name}
-      />
+      <div className="table">
+        {tracks_data.map((data, i) => (
+          <TrackCard
+            img={data.album.images[0].url}
+            name={data.name}
+            artists={data.artists}
+            album_name={data.album.name}
+            key={i}
+          />
+        ))}
+      </div>
     </div>
   );
 }
