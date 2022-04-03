0af2cd5c1d29249009ea30404886c0fd90af314f
stackoverprof <mail.errbint@gmail.com>
2022-04-04 06:44:50 +0700

    feat: playlist creation working

--- src/components/CreatePlaylist/index.jsx
+++ src/components/CreatePlaylist/index.jsx
@@ -1,10 +1,37 @@
 import React, { useState } from "react";
+import axiosSpotify from "../../utils/axios-spotify";
 
-const CreatePlaylist = ({ selectedTracks }) => {
+const CreatePlaylist = ({ selectedTracks, user }) => {
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
 
-  const handleCreatePlaylist = () => {};
+  const handleCreatePlaylist = async (e) => {
+    e.preventDefault();
+    if (!selectedTracks.length) return;
+    const uris = selectedTracks;
+
+    // create a new playlist
+    const playlist_id = await axiosSpotify
+      .post(`/users/${user.id}/playlists`, {
+        name: title,
+        description: description,
+        public: false,
+        collaborative: false,
+      })
+      .then((res) => res.data.id)
+      .catch((err) => err.response.data);
+
+    // add the selected tracks to the playlist
+    const result = await axiosSpotify
+      .post(`/playlists/${playlist_id}/tracks`, {
+        uris,
+      })
+      .then((res) => res.data.snapshot_id)
+      .catch((err) => console.error(err.response.data));
+
+    // confirm success
+    alert("playlist created: " + result);
+  };
 
   return (
     <form
