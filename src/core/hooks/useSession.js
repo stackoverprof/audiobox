1d8900a2a44748cdd5788f33dbf13ad0364d3da9
stackoverprof <mail.errbint@gmail.com>
2022-04-08 21:00:58 +0700

    fix: fixing swr issue

--- src/core/hooks/useSession.js
+++ src/core/hooks/useSession.js
@@ -1,16 +1,13 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import useUser from 'src/core/swr/user';
 import { syncToken } from 'src/core/redux/actions/auth';
 
 const useSession = () => {
 	const dispatch = useDispatch();
-	const { mutate } = useUser();
 
 	return useEffect(() => {
 		dispatch(syncToken());
-		mutate();
-	}, [dispatch, mutate]);
+	}, [dispatch]);
 };
 
 export default useSession;
--- src/core/swr/user.js
+++ src/core/swr/user.js
@@ -1,5 +1,7 @@
-import useSWR from 'swr';
+import useSWR, { useSWRConfig } from 'swr';
 import axiosSpotify from 'src/core/utils/axios-spotify';
+import { useAuth } from '../redux/reducer/auth';
+import { useEffect } from 'react';
 
 const userFetcher = async () => {
 	const result = await axiosSpotify.get('/me').then((res) => res.data);
@@ -7,7 +9,14 @@ const userFetcher = async () => {
 };
 
 export default function useUser() {
+	const { cache } = useSWRConfig();
 	const { data, mutate, error } = useSWR('api_user', userFetcher);
+	const { authenticated } = useAuth();
+
+	useEffect(() => {
+		if (authenticated) mutate();
+		else cache.delete('api_user');
+	}, [authenticated]);
 
 	return {
 		loading: !data && !error,
