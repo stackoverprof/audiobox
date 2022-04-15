b60f429465ee4903cc1ef0923c9a060b88d418fe
stackoverprof <mail.errbint@gmail.com>
2022-04-16 01:18:29 +0700

    fix: update spotify login

--- src/core/utils/spotify-login.ts
+++ src/core/utils/spotify-login.ts
@@ -3,18 +3,13 @@ import randomBytesJs from 'random-bytes-js';
 const client_id = import.meta.env.VITE_SPOTIFY_CLIENT_ID;
 
 const spotifyLogin = () => {
-	const redirect_uri = window.location.origin;
-	const state = randomBytesJs.randHex(8);
-
-	localStorage.setItem('spotify-client', state);
-	const scope = 'playlist-modify-private';
-
 	const targetURL = new URL('https://accounts.spotify.com/authorize');
+
 	targetURL.searchParams.set('response_type', 'token');
 	targetURL.searchParams.set('client_id', client_id);
-	targetURL.searchParams.set('redirect_uri', redirect_uri);
-	targetURL.searchParams.set('state', state);
-	targetURL.searchParams.set('scope', scope);
+	targetURL.searchParams.set('redirect_uri', window.location.origin);
+	targetURL.searchParams.set('state', randomBytesJs.randHex(8));
+	targetURL.searchParams.set('scope', 'playlist-modify-private');
 
 	return targetURL.href;
 };
