824966cbfe834980f8fc4addfec7681f7213b3c1
stackoverprof <mail.errbint@gmail.com>
2022-04-16 00:50:10 +0700

    fix: update user fetcher

--- src/core/api/fetchers/index.ts
+++ src/core/api/fetchers/index.ts
@@ -2,14 +2,16 @@ import Spotify from '../lib/spotify';
 import SpotifyPublic from '../lib/spotifyPublic';
 
 export const getUser = async () => {
-	const result = await Spotify.get('/me').then((res) => res.data);
+	const result = await Spotify.get('/me')
+		.then((res) => res.data)
+		.catch((err) => console.error(err.response.data));
 	return result;
 };
 
 export const getRecommendations = async () => {
 	const result = await SpotifyPublic.get('/playlists/37i9dQZEVXbMDoHDwVN2tF')
 		.then((res) => res.data)
-		.catch((err) => console.log(err.response.data));
+		.catch((err) => console.error(err.response.data));
 
 	return result;
 };
