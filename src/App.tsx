c6f180c1e4e3110096d52983f7e665ffed42a8be
stackoverprof <mail.errbint@gmail.com>
2022-04-15 09:03:45 +0700

    feat: eslint and prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,46 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./core/style/tailwind.css";
-import "./core/style/typefaces.css";
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './core/style/tailwind.css';
+import './core/style/typefaces.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+	const [count, setCount] = useState(0);
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="h-40 w-40" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="w-40 h-40" alt="logo" />
+				<p>Hello Vite + React!</p>
+				<p>
+					<button type="button" onClick={() => setCount((count) => count + 1)}>
+						count is: {count}
+					</button>
+				</p>
+				<p>
+					Edit <code>App.tsx</code> and save to test HMR updates.
+				</p>
+				<p>
+					<a
+						className="App-link"
+						href="https://reactjs.org"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Learn React
+					</a>
+					{' | '}
+					<a
+						className="App-link"
+						href="https://vitejs.dev/guide/features.html"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Vite Docs
+					</a>
+				</p>
+			</header>
+		</div>
+	);
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import App from './App';
+import ReactDOM from 'react-dom/client';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+const Root = document.getElementById('root');
+
+if (Root) {
+	ReactDOM.createRoot(Root).render(
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	);
+}
