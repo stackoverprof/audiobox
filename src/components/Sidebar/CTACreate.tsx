c3627fd86a952a731cbc86719d087d0c67e267c2
stackoverprof <mail.errbint@gmail.com>
2022-04-16 23:40:12 +0700

    feat: basic sidebar styling #2

--- src/components/Sidebar/CTACreate.tsx
+++ src/components/Sidebar/CTACreate.tsx
@@ -7,10 +7,10 @@ const CTACreate = () => {
 		<div className="px-6 my-8 w-full">
 			<Link
 				to="/create"
-				className="flex-bc py-3 pr-3 pl-4 w-full bg-opacity-70 rounded-md bg-theme-pink"
+				className="flex-bc py-3 pr-3 pl-4 w-full bg-opacity-70 rounded-md transition group hover:bg-opacity-60 bg-theme-pink"
 			>
 				<p className="text-lg">Create</p>
-				<div className="flex-cc w-9 h-9 bg-white bg-opacity-20 rounded-md">
+				<div className="flex-cc w-9 h-9 bg-white bg-opacity-20 rounded-md transition-all transform group-hover:bg-theme-pink group-hover:bg-opacity-100 group-hover:scale-110">
 					<FiPlus className="text-white" size={24} />
 				</div>
 			</Link>
