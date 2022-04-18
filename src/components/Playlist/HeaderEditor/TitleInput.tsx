8e4ac3150d57c25bb79895312a0295b1346fe885
stackoverprof <mail.errbint@gmail.com>
2022-04-18 16:59:18 +0700

    feat: Untitled Placeholder

--- src/components/Playlist/HeaderEditor/TitleInput.tsx
+++ src/components/Playlist/HeaderEditor/TitleInput.tsx
@@ -25,9 +25,13 @@ const TitleInput = ({ value, onChange, disabled }: Props) => {
 			/>
 			<div className="absolute flex-sc pl-7 pointer-events-none full">
 				<div className="flex-ce text-5xl font-semibold">
-					<span className="opacity-0">{value}</span>
-					{!focus && !disabled && (
-						<FiEdit3 size={32} className="mb-0.5 ml-3 opacity-40" />
+					{!value && (
+						<div className="flex-ce text-5xl font-semibold">
+							<span className="opacity-30">Untitled </span>
+							{!focus && !disabled && (
+								<FiEdit3 size={32} className="mb-0.5 ml-3 opacity-40" />
+							)}
+						</div>
 					)}
 				</div>
 			</div>
