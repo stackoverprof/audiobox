57e42d6671fbed73b71f989ede9643f09cc8910f
stackoverprof <mail.errbint@gmail.com>
2022-04-19 00:51:49 +0700

    fix: noneed to delete userData cache

--- src/core/swr/userData.ts
+++ src/core/swr/userData.ts
@@ -1,17 +1,8 @@
-import { useEffect } from 'react';
 import * as fetchers from '@core/api/fetchers';
-import useSWR, { useSWRConfig } from 'swr';
-import { useAuth } from '../redux/reducer/auth';
+import useSWR from 'swr';
 
 const useUserData = () => {
-	const { cache } = useSWRConfig();
 	const { data, mutate, error } = useSWR('api_user', fetchers.getUser);
-	const { authenticated } = useAuth();
-
-	useEffect(() => {
-		if (authenticated) mutate();
-		else cache.delete('api_user');
-	}, [authenticated]);
 
 	return {
 		loading: !data && !error,
