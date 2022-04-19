5ccb48dc79bd4caaed3ff33a43bd332c99b286d4
stackoverprof <mail.errbint@gmail.com>
2022-04-19 13:16:56 +0700

    fix: hide add tracks button on public playlist

--- src/components/Playlist/ContentTracks/ButtonOptions.tsx
+++ src/components/Playlist/ContentTracks/ButtonOptions.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import usePlaylist from '@core/swr/usePlaylist';
 import { HiPlus } from 'react-icons/hi';
-import { setEditMode } from '@core/redux/reducer/editPlaylist';
+import { setEditMode, useEditPlaylist } from '@core/redux/reducer/editPlaylist';
 import { useDispatch } from 'react-redux';
 
 const ButtonOptions = () => {
+	const { id } = useEditPlaylist();
+	const { isOwned } = usePlaylist(id);
 	const dispatch = useDispatch();
+
+	if (!isOwned) return <></>;
 	return (
 		<div className="flex-cc">
 			<button
-				onClick={() => dispatch(setEditMode(true))}
+				onClick={() => (isOwned ? dispatch(setEditMode(true)) : {})}
 				className="flex-cc ml-2 w-8 h-8 rounded-md border border-opacity-50 text-theme-blue hover:bg-theme-blue hover:bg-opacity-20 hover:text-white border-theme-blue"
 			>
 				<HiPlus size={24} />
--- src/components/Playlist/ContentTracks/TrackCardAdder.tsx
+++ src/components/Playlist/ContentTracks/TrackCardAdder.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import usePlaylist from '@core/swr/usePlaylist';
 import { HiPlus } from 'react-icons/hi';
-import { setEditMode } from '@core/redux/reducer/editPlaylist';
+import { setEditMode, useEditPlaylist } from '@core/redux/reducer/editPlaylist';
 import { useDispatch } from 'react-redux';
 
 const TrackCardAdder = () => {
+	const { id } = useEditPlaylist();
+	const { isOwned } = usePlaylist(id);
 	const dispatch = useDispatch();
+
+	if (!isOwned) return <></>;
 	return (
 		<button
-			onClick={() => dispatch(setEditMode(true))}
-			className="h-28 transition w-full border border-white group rounded-md hover:bg-white hover:bg-opacity-5 flex-cc border-opacity-20"
+			onClick={() => (isOwned ? dispatch(setEditMode(true)) : {})}
+			className="flex-cc w-full h-28 rounded-md border border-white border-opacity-20 transition group hover:bg-white hover:bg-opacity-5"
 		>
 			<HiPlus
 				size={40}
-				className="text-white opacity-30 group-hover:opacity-100 transition"
+				className="text-white opacity-30 transition group-hover:opacity-100"
 			/>
 		</button>
 	);
