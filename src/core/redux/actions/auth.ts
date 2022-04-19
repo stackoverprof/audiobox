06569c71a151b8a9fe2e52840e4b1dfefb45a894
stackoverprof <mail.errbint@gmail.com>
2022-04-19 13:44:01 +0700

    fix: cancel reload post logout

--- src/core/redux/actions/auth.ts
+++ src/core/redux/actions/auth.ts
@@ -38,5 +38,4 @@ export const logout = () => async (dispatch) => {
 	localStorage.removeItem('access_token');
 	localStorage.removeItem('token_expiration');
 	await dispatch(removeToken());
-	window.location.reload();
 };
