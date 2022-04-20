acbc742ca69f89debde17ba534b37ce008d578d9
stackoverprof <mail.errbint@gmail.com>
2022-04-20 21:44:08 +0700

    Revert "feat: a working vitest test"
    This reverts commit ffc6edc546c8776b7be3cfd7c03b208934818159.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { configDefaults, defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: './setupTests.ts',
-		exclude: [...configDefaults.exclude, 'src/components/_shared/Lottie/**'],
-	},
 });
