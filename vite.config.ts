ffc6edc546c8776b7be3cfd7c03b208934818159
stackoverprof <mail.errbint@gmail.com>
2022-04-20 07:06:13 +0700

    feat: a working vitest test

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
-import { defineConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), tsconfigPaths()],
+	test: {
+		globals: true,
+		environment: 'jsdom',
+		setupFiles: './setupTests.ts',
+		exclude: [...configDefaults.exclude, 'src/components/_shared/Lottie/**'],
+	},
 });
