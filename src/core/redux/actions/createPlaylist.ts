a9b59011892ba4fd86613347379407d4bb244005
stackoverprof <mail.errbint@gmail.com>
2022-04-20 21:57:32 +0700

    fix: calling store as func

--- src/core/redux/actions/createPlaylist.ts
+++ src/core/redux/actions/createPlaylist.ts
@@ -4,13 +4,13 @@ import store from '../store';
 import { setSelectedTracks, TrackType } from '../reducer/createPlaylist';
 
 export const addTrack = (track: TrackType) => (dispatch) => {
-	const { selectedTracks } = store.getState().createPlaylist;
+	const { selectedTracks } = store().getState().createPlaylist;
 	const added = [track, ...selectedTracks];
 	dispatch(setSelectedTracks(added));
 };
 
 export const removeTrack = (uri: string) => (dispatch) => {
-	const { selectedTracks } = store.getState().createPlaylist;
+	const { selectedTracks } = store().getState().createPlaylist;
 	const removed = [...selectedTracks].filter((track) => track.uri !== uri);
 	dispatch(setSelectedTracks(removed));
 };
